6617ef1a4b9b543afad581416af6d3041a72113d
ponasaugis <ponasaugis@gmail.com>
2021-12-07 19:30:26 +0200

    10

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 let burgerBtn = document.getElementById('burgerBtn')
 let topMenu = document.getElementById('topMenu')
-let active = document.getElementsByClassName('.active')
+// let active = document.getElementsByClassName('.active')
 let activeNav = document.getElementById('nav')
-// let test = document.getElementsByClassName('nav')
 
-console.log(activeNav)
 
 burgerBtn.addEventListener('click', function () {
 	topMenu.classList.toggle('active')
 	activeNav.classList.toggle('activeBg')
-
-	// activeNav.classList.toggle('navBackground')
-	// activeNav.style.backgroundColor = 'rgba(86,83,76, 0.95)'
-	console.log('paspaudziau')
-	
+		
 });
