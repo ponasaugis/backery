0a5ad603279a28f81f0f97c84f87e59d7739f28f
ponasaugis <ponasaugis@gmail.com>
2021-12-09 19:41:21 +0200

    Modified navbar

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let burgerBtn = document.getElementById('burgerBtn')
 let topMenu = document.getElementById('topMenu')
-// let active = document.getElementsByClassName('.active')
 let activeNav = document.getElementById('nav')
+let burgerBtninn = document.querySelector(".burgerBtnInner")
 
+let ghostLetter = document.querySelector(".ghostLetter")
+let readBtn = document.getElementById('readBtn')
+let closeBtn = document.getElementById('closeBtn')
+console.log(readBtn)
 
 burgerBtn.addEventListener('click', function () {
 	topMenu.classList.toggle('active')
 	activeNav.classList.toggle('activeBg')
+	// activeNav.style.backgroundColor = 'blue';
+	burgerBtninn.classList.toggle('active')
+	console.log(activeNav.classList)
 		
 });
+
+readBtn.addEventListener('click', function () {
+	ghostLetter.classList.toggle('ghostLetterActive')
+	console.log('paspaudziau')
+})
+
+closeBtn.addEventListener('click', function () {
+	ghostLetter.classList.toggle('ghostLetterActive')
+	console.log('paspaudziau')
+})
